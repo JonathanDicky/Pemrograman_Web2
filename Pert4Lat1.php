e573e62f1c341b3947701aaa888eca86c38ed7c2
JonathanDicky <jonthzee@gmail.com>
2026-09-17 20:02:58 +0700

    Pertemuan 4

---
+++ Pert4Lat1.php
@@ -0,0 +1,27 @@
+<?php
+
+$brush_price = 5;
+$counter = 10;
+
+echo "<table border=\"1\" align=\"center\">";
+echo "<tr>";
+echo "<th>Quantity</th>";
+echo "<th>Price</th>";
+echo "</tr>";
+
+while ($counter <= 100) {
+    echo "<tr>";
+    echo "<td>";
+    echo $counter;
+    echo "</td>";
+    echo "<td>";
+    echo $brush_price * $counter;
+    echo "</td>";
+    echo "</tr>";
+
+    $counter = $counter + 10;
+}
+
+echo "</table>";
+
+?>
